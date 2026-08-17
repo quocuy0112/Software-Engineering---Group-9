14eee0dc58766ae2044dddd58acb7cbcb008a858
Dava <phucrom105@gmail.com>
2026-08-17 15:45:08 +0700

    Delete local migration

--- web/prisma/migrations/20260816050940_smarthire/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX "JobPostReviewAggregate_pendingVersionId_version_idx" ON "JobPostReviewAggregate"("pendingVersionId", "version");
-
--- RenameIndex
-ALTER INDEX "JobPostReviewHistory_reviewVersionId_resultingAggregateVersion_" RENAME TO "JobPostReviewHistory_reviewVersionId_resultingAggregateVers_key";
